9cf1102e8dfb53f9cb058525ba8dfc46540b5c6f
fadel100 <fadel.ibrahim.it@gmail.com>
2019-05-20 11:11:34 +0300

    Step 4

---
+++ Exercises/Javascript-Basics-02/step 4/step4.js
@@ -0,0 +1,16 @@
+// targeting the button element
+var target_button = document.querySelector("button");
+
+ 
+// add an event to the button
+target_button.addEventListener("click", function(){
+    // dialogue yes or no
+    var yes_or_no = prompt("do u want to reset yes or no?!");
+
+    if(yes_or_no == "yes"){
+        // change value of inputs
+        document.querySelector("#name").value = "";
+        document.querySelector("#surname").value = "";
+        document.querySelector("#city").value = "";
+    }
+},false);
