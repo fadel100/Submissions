7410b70dfb600d62f25abbaa3521817a97b4ae1a
fadel100 <fadel.ibrahim.it@gmail.com>
2019-06-11 13:24:24 +0300

    Step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -3,6 +3,25 @@ const app = express()
 const port = 3000
 
 // the root URL (/)
-app.get('/', (req, res) => res.send('ok'))
+app.get('/', (req, res) => res.send("ok"));
+
+
+app.get('/test',(req, res) => res.send({
+    status: 200,
+    message: "ok"
+}));
+
+
+
+// create a Date instance
+var today = new Date();
+var current_time = today.getHours()+":"+today.getMinutes();
+
+
+app.get('/time',(req, res) => res.send({
+    status: 200,
+    message: current_time
+}));
+
 
 app.listen(port, () => (console.log("app is running")));
