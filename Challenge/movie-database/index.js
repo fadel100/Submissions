e20bc79ae0884562c74d7af65b0a9088453df7fb
fadel100 <fadel.ibrahim.it@gmail.com>
2019-06-12 09:25:16 +0300

    Step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -24,4 +24,32 @@ app.get('/time',(req, res) => res.send({
 }));
 
 
+
+app.get('/hello/:id', (req, res) => {
+    var id = req.params.id;
+    res.send({
+        status: 200,
+        message: "Hello, "+id+""
+    })
+})
+
+
+// when we call /search?s=something , something will be assigned to s
+app.get('/search', (req, res) => {
+    var searchInput = req.query.s;
+    if(searchInput){
+    res.send({
+        status: 200,
+        message: "ok",
+        data: searchInput
+    })
+    }else{
+        res.send({
+            status: 500,
+            error: true,
+            message: "you have to provide a search",
+        })
+    }
+})
+
 app.listen(port, () => (console.log("app is running")));
