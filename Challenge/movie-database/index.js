64d820dd23f092f116e0290d3d7ccc2d365c6eb6
fadel100 <fadel.ibrahim.it@gmail.com>
2019-06-12 10:29:28 +0300

    Step 5 - Set up the basis for CRUD

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -52,4 +52,33 @@ app.get('/search', (req, res) => {
     }
 })
 
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+
+// create
+app.get('/movies/add',(req, res) => res.send("asdasd"));
+// read
+app.get('/movies/get', (req, res) => res.send({status:200, data: movies }));
+// update
+app.get('/movies/edit',(req, res) => res.send("asdasd"));
+// delete
+app.get('/movies/delete',(req, res) => res.send("asdasd"));
+
+
+// for later use
+// app.get('/movies/get', (req, res)=>{
+//     for(let i=0;i<movies.length;i++){
+//         res.write(JSON.stringify({status: 200, data: movies[i].title}))
+//         res.write("\n")
+//     }
+//     res.end();
+// })
+
+
 app.listen(port, () => (console.log("app is running")));
