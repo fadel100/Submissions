5a1f8e91b2dcabd30dc1e29a636c1ccaba4a745c
fadel100 <fadel.ibrahim.it@gmail.com>
2019-06-14 12:30:23 +0300

    react only

---
+++ Exercises/portfolio/src/components/resume/resume.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+export default class Resume extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <section id="resume">
+  {/* Education
+----------------------------------------------- */}
+  <div className="row education">
+    <div className="three columns header-col">
+      <h1><span>Education</span></h1>
+    </div>
+    <div className="nine columns main-col">
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>AOU</h3>
+          <p className="info">Computer science <span>•</span> <em className="date">April 2007</em></p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+            ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
+          </p>
+        </div>
+      </div> {/* item end */}
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>ITI</h3>
+          <p className="info">TS in systems and networks <span>•</span> <em className="date">March 2003</em></p>
+          <p>
+            This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+            Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
+            nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
+            ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat
+          </p>
+        </div>
+      </div> {/* item end */}
+    </div> {/* main-col end */}
+  </div> {/* End Education */}
+  {/* Work
+----------------------------------------------- */}
+  <div className="row work">
+    <div className="three columns header-col">
+      <h1><span>Work</span></h1>
+    </div>
+    <div className="nine columns main-col">
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>a company</h3>
+          <p className="info">ajksdnklashjdi <span>•</span> <em className="date">March 2010 - Present</em></p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+            ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
+          </p>
+        </div>
+      </div> {/* item end */}
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>company 2</h3>
+          <p className="info">asdasd <span>•</span> <em className="date">March 2007 - February 2010</em></p>
+          <p>
+            This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+            Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
+            nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
+            ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat
+          </p>
+        </div>
+      </div> {/* item end */}
+    </div> {/* main-col end */}
+  </div> {/* End Work */}
+  {/* Skills
+----------------------------------------------- */}
+  <div className="row skill">
+    <div className="three columns header-col">
+      <h1><span>Skills</span></h1>
+    </div>
+    <div className="nine columns main-col">
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+        eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+        voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt.
+      </p>
+      <div className="bars">
+        <ul className="skills">
+          <li><span className="bar-expand photoshop" /><em>html</em></li>
+          <li><span className="bar-expand illustrator" /><em>css</em></li>
+          <li><span className="bar-expand wordpress" /><em>javascript</em></li>
+          <li><span className="bar-expand css" /><em>express.js</em></li>
+          <li><span className="bar-expand html5" /><em>nodejs</em></li>
+          <li><span className="bar-expand jquery" /><em>react</em></li>
+        </ul>
+      </div>{/* end skill-bars */}
+    </div> {/* main-col end */}
+  </div> {/* End skills */}
+</section>
+
+      </React.Fragment>
+    );
+  }
+}
